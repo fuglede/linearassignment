4458611b0e8ea63774543a2a7de8f58dda0e88e7
Søren Fuglede Jørgensen <s@fuglede.dk>
2019-07-13 22:12:37 +0200

    Reintroduce general solver class as a main entry point for the library

---
+++ src/LinearAssignment/Solver.cs
@@ -0,0 +1,12 @@
+﻿namespace LinearAssignment
+{
+    public class Solver
+    {
+        public static Assignment Solve(double[,] cost, bool maximize = false, bool skipPositivityTest = false) =>
+            ShortestPath.Solve(cost, maximize, skipPositivityTest);
+
+        // TODO: Align interfaces between double and int case
+        public static Assignment Solve(int[,] cost) =>
+            Pseudoflow.Solve(cost);
+    }
+}
