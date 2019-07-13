1df8e0bc5b510521e70d0bc1f0e081113fe087c5
Søren Fuglede Jørgensen <s@fuglede.dk>
2019-07-13 22:30:46 +0200

    Add comment on usage

--- src/LinearAssignment/Solver.cs
+++ src/LinearAssignment/Solver.cs
@@ -1,16 +1,21 @@
 ﻿namespace LinearAssignment
 {
+    /// <summary>
+    /// General solver of the linear assignment problem that provides sane choices
+    /// of defaults for algorithms. This is the intended entry point for general
+    /// purpose usage of the library.
+    /// </summary>
     public class Solver
     {
         public static Assignment Solve(double[,] cost, ISolver solver = null)
         {
-            if (solver == null) solver = new ShortestPath();
+            if (solver == null) solver = new ShortestPathSolver();
             return solver.Solve(cost);
         }
 
         public static Assignment Solve(int[,] cost, ISolver solver = null)
         {
-            if (solver == null) solver = new Pseudoflow();
+            if (solver == null) solver = new PseudoflowSolver();
             return solver.Solve(cost);
         }
     }
