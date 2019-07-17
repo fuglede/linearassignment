9306a8653d9f752570ce35fc862c6198958a6416
Søren Fuglede Jørgensen <s@fuglede.dk>
2019-07-17 23:28:27 +0200

    Add an implementation of CSR sparse matrices

---
+++ src/LinearAssignment.Tests/SparseMatrixTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LinearAssignment.Tests
+{
+    public class SparseMatrixTest
+    {
+        [Fact]
+        public void SparseMatrixConstructsProperlyForDenseIntegralInput()
+        {
+            int[,] dense = {{0, 0, 0, 0}, {5, 8, 0, 0}, {0, 0, 3, 0}, {0, 6, 0, 0}};
+            var sparse = new SparseMatrix(dense, 0);
+            Assert.Equal(sparse.A, new List<int> {5, 8, 3, 6});
+            Assert.Equal(sparse.IA, new List<int> {0, 0, 2, 3, 4});
+            Assert.Equal(sparse.CA, new List<int> {0, 1, 2, 1});
+        }
+    }
+}
---
+++ src/LinearAssignment/SparseMatrix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinearAssignment
+{
+    /// <summary>
+    /// Represents a sparse matrix in compressed sparse row (CSR) format.
+    /// </summary>
+    public class SparseMatrix
+    {
+        public SparseMatrix(int[,] dense, int empty = int.MaxValue)
+        {
+            A = new List<int>();
+            IA = new List<int>();
+            CA = new List<int>();
+            IA.Add(0);
+            int nonInfinite = 0;
+            var nr = dense.GetLength(0);
+            var nc = dense.GetLength(1);
+            for (int i = 0; i < nr; i++)
+            {
+                for (int j = 0; j < nc; j++)
+                {
+                    int entry = dense[i, j];
+                    if (!entry.Equals(empty))
+                    {
+                        nonInfinite++;
+                        A.Add(entry);
+                        CA.Add(j);
+                        if (entry > _max)
+                            _max = entry;
+                    }
+                }
+                IA.Add(nonInfinite);
+            }
+
+            NumRows = nr;
+            NumColumns = nc;
+        }
+
+        private int _max = int.MinValue;
+        public int MaxValue => _max != int.MaxValue ? _max : _max = A.Max();
+        public List<int> A { get; }
+        public List<int> IA { get; }
+        public List<int> CA { get; }
+
+        public int NumRows { get; }
+        public int NumColumns { get; }
+    }
+}
